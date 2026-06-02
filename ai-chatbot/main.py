9a4dc585640e066db01f78c4e5a1380df72d1851
Suraj Deshmukh <suraj.d@sodelsolutions.com>
2026-06-02 12:56:51 +0530

    Initial commit

---
+++ ai-chatbot/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "AI Chatbot Backend Running"}
+
+@app.get("/ask")
+def ask_ai(prompt: str):
+
+    response = requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": "llama3.2",
+            "prompt": prompt,
+            "stream": False
+        }
+    )
+
+    result = response.json()
+
+    return {
+        "prompt": prompt,
+        "response": result["response"]
+    }
